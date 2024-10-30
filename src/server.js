5946190a9d6f6763f6e6304190e17989424075b8
reshameir <reshameiranadi@gmail.com>
2024-10-30 17:16:47 +0700

    update code

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['http://notesapp-v1.dicodingacademy.com'],
+                credentials: true  // untuk mengizinkan penggunaan cookie jika diperlukan
+            },
+        },
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
