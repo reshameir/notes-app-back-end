dfc601a2512b01e7351980111d41756297bf2d77
reshameir <reshameiranadi@gmail.com>
2025-01-15 09:15:33 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
+    const notesService = new NotesService();
+
     const server = Hapi.server({
         port: 5000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -12,7 +17,30 @@ const init = async () => {
         },
     });
 
-    server.route(routes);
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+            validator: NotesValidator,
+        },
+    });
+
+    server.ext('onPreResponse', (request, h) => {
+        // mendapatkan konteks response dari request
+        const { response } = request;
+
+        // penanganan client error secara internal.
+        if (response instanceof ClientError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message,
+            });
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
+
+        return h.continue;
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
